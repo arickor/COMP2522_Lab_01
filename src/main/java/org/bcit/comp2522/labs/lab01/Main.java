7787ff4edc9d54b4515716ba257a463daf6afea8
arickor <113308237+arickor@users.noreply.github.com>
2023-01-07 18:56:38 -0800

    fixed Checkstyle errors

--- src/main/java/org/bcit/comp2522/labs/lab01/Main.java
+++ src/main/java/org/bcit/comp2522/labs/lab01/Main.java
@@ -1,7 +1,15 @@
 package org.bcit.comp2522.labs.lab01;
 
+/**
+ * Main.java
+ * Prints out my favourite animal.
+ *
+ * @author Aric Or
+ * @version 1
+ *
+ */
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("My favourite animal is the giant panda!");
-    }
+  public static void main(String[] args) {
+    System.out.println("My favourite animal is the giant panda!");
+  }
 }
